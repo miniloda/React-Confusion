cf3aceea5fdf9a839fba571a2c070dcce75a3081
Jacob maximus Usaraga <jlusaraga@up.edu.ph>
2022-09-07 07:04:42 +0800

    init maincomponent.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import {Navbar, NavbarBrand, Nav, NavItem, NavLink} from 'reactstrap';
-
-function App() {
-  return (
-    <div className="App">
+import Menu from "./components/MenuComponent";
+import {DISHES} from "./shared/dishes";
+import { Component } from 'react';
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    }
+  }
+  render(){
+    return (
+    <div>
       <Navbar dark color="primary">
+        <div className="container">
         <NavbarBrand href="/"> Ristorante Con Fusion</NavbarBrand>
+
+        </div>
         </Navbar>
+        <Menu dishes = {this.state.dishes}/>
     </div>
   );
+    }
 }
 
 export default App;
