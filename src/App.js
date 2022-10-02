d90258229fd86b72df3cc29debacf3115d23b7fe
Jacob maximus Usaraga <jlusaraga@up.edu.ph>
2022-10-03 03:52:00 +0800

    added router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import {Navbar, NavbarBrand, Nav, NavItem, NavLink} from 'reactstrap';
 import { Component } from 'react';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 class App extends Component{
   render(){
     return (
-    <div>
-      <Main />
-    </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+     </BrowserRouter>
   );
     }
 }
